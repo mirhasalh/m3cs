dc753203925ae730156685b23524fb9b64246f95
Irhas <muhammadirhasalh@gmail.com>
2024-06-23 10:40:37 +0700

    Set-up responsive layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,65 @@
 <script>
-  let primaryColor = '#4b0082',
-    secondaryColor = '#fc6d26'
+  import Palette from '$lib/Palette.svelte'
 
   const n = 33
+
+  let primaryColor = '#4b0082',
+    secondaryColor = '#fc6d26'
 </script>
 
-<div class={`color-bar-wrapper`}>
-  <div class={`color-bar`}>
-    <div class="color-pick">
-      <input type="color" bind:value={primaryColor} />
-    </div>
-    <div>
-      <h3 class={`shrink`}>Primary</h3>
-      <p class={`shrink`}><small>{primaryColor}</small></p>
-    </div>
+<header class={`app-bar`}>
+  <Palette color={primaryColor} />
+</header>
+<div class={`app-shell`}>
+  <div></div>
+  <div>
+    <main class={`main-content`}>
+      <div>
+        <div class={`color-bar`}>
+          <div class="color-pick">
+            <input type="color" bind:value={primaryColor} />
+          </div>
+          <div>
+            <p class={`shrink`}><strong>Primary</strong></p>
+            <p class={`shrink`}><small>{primaryColor}</small></p>
+          </div>
+        </div>
+        <hr class={`transparent`} />
+        <div class={`color-bar`}>
+          <div class="color-pick">
+            <input type="color" bind:value={secondaryColor} />
+          </div>
+          <div>
+            <p class={`shrink`}><strong>Secondary</strong></p>
+            <p class={`shrink`}><small>{secondaryColor}</small></p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <section class={`color-scheme-wrapper`}>
+          <header>
+            <h3>Light scheme</h3>
+          </header>
+          <div class={`color-grid`}>
+            {#each Array.from({ length: n }) as item}
+              <div class={`color-grid-item`}>{item}</div>
+            {/each}
+          </div>
+        </section>
+        <hr class={`transparent`} />
+        <section class={`color-scheme-wrapper`}>
+          <header>
+            <h3>Dark scheme</h3>
+          </header>
+          <div class={`color-grid`}>
+            {#each Array.from({ length: n }) as item}
+              <div class={`color-grid-item`}>{item}</div>
+            {/each}
+          </div>
+        </section>
+      </div>
+    </main>
   </div>
-  <hr class={`transparent`} />
-  <div class={`color-bar`}>
-    <div class="color-pick">
-      <input type="color" bind:value={secondaryColor} />
-    </div>
-    <div>
-      <h3 class={`shrink`}>Secondary</h3>
-      <p class={`shrink`}><small>{secondaryColor}</small></p>
-    </div>
-  </div>
+  <div></div>
 </div>
-<section class={`color-scheme-wrapper`}>
-  <header>
-    <h4>Light scheme</h4>
-  </header>
-  <div class={`color-grid`}>
-    {#each Array.from({ length: n }) as item}
-      <div class={`color-grid-item`}>{item}</div>
-    {/each}
-  </div>
-</section>
-<hr class={`transparent`} />
-<section class={`color-scheme-wrapper`}>
-  <header>
-    <h4>Dark scheme</h4>
-  </header>
-  <div class={`color-grid`}>
-    {#each Array.from({ length: n }) as item}
-      <div class={`color-grid-item`}>{item}</div>
-    {/each}
-  </div>
-</section>
